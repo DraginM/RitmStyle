15a47618b00f9ead05c3fb8f9e6eb51334462fdc
DraginM <kostya2015ok@yandex.ru>
2025-04-02 16:55:10 +0300

    Add files via upload

---
+++ Scrypt/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    initialSlide: 1,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
